f4cca3823b84327ef4057d99c2f2e342e59f4844
wuwenwen <wenwenwenwu@foxmail.com>
2021-05-08 09:40:45 +0800

    新建项目

---
+++ FabulousApp/FabulousApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FabulousApp
+//
+//  Created by 邬文文 on 2021/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
